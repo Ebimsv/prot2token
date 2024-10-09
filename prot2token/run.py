ba2629731c5cf5d694addb48e34e850d9491ed40
ebi <ebimsv0501@gmail.com>
2024-10-09 18:30:54 +0330

    Add some useful prints

--- prot2token/run.py
+++ prot2token/run.py
@@ -4,17 +4,31 @@ from models import prepare_models
 
 
 def test():
+    # Clear CUDA cache
     torch.cuda.empty_cache()
+    print("CUDA cache cleared.")
 
-    net, decoder_tokenizer, inference_configs  = prepare_models(name='fluorescence', device='cuda', compile_model=True)
+    # Prepare models
+    net, decoder_tokenizer, inference_configs = prepare_models(name="stability", device="cpu", compile_model=False)
+    print("Models prepared:")
+    print(f"Net: {net}")
+    print(f"Decoder Tokenizer: {decoder_tokenizer}")
+    print(f"Inference Configs: {inference_configs}")
 
-    samples = pd.read_csv('../inference_data/fluorescence_inference.csv')['input'].tolist()
+    # Load samples from CSV
+    samples = pd.read_csv("../data/inference_data/stability_inference.csv")["input"].tolist()
+    print("Samples loaded:")
+    print(samples)
 
-    results = net.run(samples, merging_character='')
+    # Run inference
+    results = net.run(samples, merging_character="")
+    print("Inference results:")
     print(results)
+
+    # Clear CUDA cache again
     torch.cuda.empty_cache()
+    print("CUDA cache cleared.")
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     test()
-    print('done!')
+    print("done!")
